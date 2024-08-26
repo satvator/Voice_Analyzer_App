46ce8dc9e87cff5746fc6e4cb07f192fcd07f584
himanshut139 <himanshutiwari139.com>
2024-08-26 18:03:56 +0530

    Routes fixed

--- backend/app.py
+++ backend/app.py
@@ -303,4 +303,5 @@ def internal_error(error):
     return jsonify({'status': 'error', 'message': 'Internal server error.'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
